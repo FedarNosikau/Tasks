a7e2b9de4087a59982bd4388bfc2c720eb3b5147
Мышаня <@192.168.100.2>
2016-08-15 13:15:17 +0300

    132

--- ALittleTasks/src/by/htp4/les08/start/TaskOver.java
+++ ALittleTasks/src/by/htp4/les08/start/TaskOver.java
@@ -1,17 +1,24 @@
 package by.htp4.les08.start;
 
+//В классе A в пакете by.htp4.les08.start определить метод x, принимающий два строковых 
+//параметра и возвращающий целое число. В классе B в том же пакете переопределить метод x. 
+//Объяснить поведение java-программы при вызове метода x на объекте класса B через ссылку типа A.
+
+
+
 public class TaskOver {
 	
 	public static void main(String[]args){
 	
+		@SuppressWarnings("unused")
 		String one = "5";
+		@SuppressWarnings("unused")
 		String two = "10";
 		
 		A a = new A();
 		a.methX();
 		
 		A b = new B();
-		
 		b.methX();
 		
 		
